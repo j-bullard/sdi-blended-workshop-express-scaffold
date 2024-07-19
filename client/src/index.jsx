76a99f37b8c9ce6b80b7ca88b27a8affdd2d5e88
Jason Bullard <jason@bullard.io>
2024-07-19 14:53:14 -0500

    feat: display books from nook

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <Fragment>
-    <></>
+    <App />
   </Fragment>,
 );
