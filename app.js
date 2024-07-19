03e97ebcf1777de40adecb2d649d9302302c95b0
Jason Bullard <jason@bullard.io>
2024-07-19 07:45:59 -0500

    feat: implement handling of PATCH endpoint
    - update the book data in the dummy.csv file
    - update the book data in the csv file in the data directory that matches the id passed
    - return the updated object should be sent back with the response

--- app.js
+++ app.js
@@ -66,4 +66,64 @@ app.get("/books/:id", (req, res) => {
   });
 });
 
+app.patch("/books/:id", (req, res) => {
+  const id = req.params.id;
+  const { title, author, cover, genres, synopsis } = req.body;
+
+  // Read data/[id].csv
+  const csvData = fs.readFileSync(
+    path.join(__dirname, "data", `${id}.csv`),
+    "utf8",
+  );
+  const books = parse(csvData, {
+    columns: true,
+    skip_empty_lines: true,
+    cast: true,
+  });
+
+  const book = books[0];
+  book.title = title || book.title;
+  book.author = author || book.author;
+  book.genres = genres?.join(", ") || book.genres;
+  book.synopsis = synopsis || book.synopsis;
+
+  const stringBook = stringify([book], { quoted: true, header: true });
+
+  fs.writeFileSync(
+    path.join(__dirname, "data", `${id}.csv`),
+    stringBook,
+    "utf8",
+  );
+
+  // Read dummy.csv
+  const dummyCsvData = fs.readFileSync(DUMMY_DATA_PATH, "utf8");
+  const dummyBooks = parse(dummyCsvData, {
+    columns: true,
+    skip_empty_lines: false,
+  });
+
+  const dummyBookIndex = dummyBooks.findIndex((b) => b.id === id);
+  const dummyBook = dummyBooks[dummyBookIndex];
+  dummyBook.title = title || dummyBook.title;
+  dummyBook.author = author || dummyBook.author;
+  dummyBook.cover = cover || dummyBook.cover;
+
+  dummyBooks[dummyBookIndex] = dummyBook;
+
+  const stringDummyBooks = stringify(dummyBooks, {
+    quoted: true,
+    header: true,
+  });
+
+  fs.writeFileSync(DUMMY_DATA_PATH, stringDummyBooks, "utf8");
+
+  res.status(200).json({
+    book: {
+      ...dummyBook,
+      genres: book.genres.split(", "),
+      synopsis: book.synopsis,
+    },
+  });
+});
+
 module.exports = app;
